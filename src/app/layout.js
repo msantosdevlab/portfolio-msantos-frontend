50eb06ecf25ab85a439d7a0811095346a4fa5fd7
msantos-lab <msantosdev.lab@gmail.com>
2025-02-04 14:30:03 +0100

    Add the class “dark” to the body

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body 
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
