9c3d368aa61b22eea2703de2c5f34f9ee7e0a108
msantos-lab <msantosdev.lab@gmail.com>
2025-02-06 15:10:45 +0100

    Changes to the content of “Introduction” and “LinkingProfile” sections

--- src/app/components/introduction/Introduction.js
+++ src/app/components/introduction/Introduction.js
@@ -1,21 +1,20 @@
 import LayersIcon from '@mui/icons-material/Layers';
-
 export default function Introduction() {
     return (
         <section className="dark:bg-dark-primary bg-light-primary pt-28 pb-20" id="introduction">
-            <div className="container max-w-6xl mx-auto px-4 py-8 banner flex flex-col items-center">
-                <p className="dark:text-white text-black mb-2">De dónde viene,</p>
+            <div className="container max-w-6xl mx-auto px-4 md:py-8 flex flex-col items-center">
+                <p className="dark:text-gray-200  text-black mb-2">Olá, meu nome é </p>      
                 <h1 className="text-gradient text-4xl md:text-5xl font-bold mb-4">
                     Mônica Santos
                 </h1>
-                <p className="dark:text-gray-300  text-black  mb-6 max-w-2xl text-center">
-                Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene sus raices en una pieza clsica de la literatura del Latin.
+                <p className="dark:text-gray-200 text-black  mb-6 max-w-2xl text-center">
+                Com mais de uma década gerenciando projetos web, migrei para a programação para unir visão estratégica e habilidades técnicas.
                 </p>
-                <p className="dark:text-gray-300 text-black mb-6 max-w-2xl text-center">
-                Que data del año 45 antes de Cristo, haciendo que este adquiera mas de 2000 años de antiguedad. Richard McClintock, un profesor de Latin de la Universidad de Hampden-Sydney en Virginia.
+                <p className="dark:text-gray-200 text-black mb-6 max-w-2xl text-center">
+                Em 2024, concluí o Curso Técnico em Programação de Sistemas e, desde então, venho aprimorando minhas habilidades com projetos práticos.
                 </p>
-                <a href="#portfolio" className="btn-default">
-                    <p> <LayersIcon sx={{ fontSize: 25 }} /> Lorem Ipsum </p>
+                <a href="#projects" className="btn-default">
+                    <p> <LayersIcon sx={{ fontSize: 25 }} /> Confira meus últimos trabalhos  </p>
                 </a>
             </div>
         </section>
--- src/app/components/linkedinprofile/LinkedinProfile.js
+++ src/app/components/linkedinprofile/LinkedinProfile.js
@@ -3,13 +3,13 @@ import { LinkedIn } from "@mui/icons-material";
 
 export default function LinkedinProfile() {
     return (
-        <section className="dark:bg-dark-primary bg-light-primary pt-20 pb-20" id="linkedinarea">
-            <div className="container max-w-6xl mx-auto px-4 py-8 flex flex-col items-center justify-between">
-                 <h2 className="text-center dark:text-gray-300 text-blackSecondary uppercase"> 
-                 Lorem Ipsum is simply dummy text
+        <section className="dark:bg-dark-primary bg-light-primary py-20" id="linkedinprofile">
+            <div className="container max-w-6xl mx-auto px-4 py-8 flex flex-col justify-between items-center">
+                 <h2 className="text-title font-title text-center dark:text-gray-300 text-blackSecondary uppercase"> 
+                    Acesse o meu <span className='text-gradient'> LinkedIn</span>!
                 </h2>
-                <p className="dark:custom-p custom-p mb-6 max-w-2xl text-center">
-                Of the printing and typesetting industry. Lorem Ipsum has been the industry.   
+                <p className="text-subtitle mb-6 max-w-2xl text-center dark:text-dark-subtitle text-light-subtitle ">
+                Quer saber mais sobre minha experiência e trajetória? <br /> Acesse meu perfil completo.
                 </p>
                 <a href="https://linkedin.com" target="_blank" className="btn-default">
                     <p> <LinkedIn sx={{ fontSize: 25 }} /> Ver Perfil</p>
