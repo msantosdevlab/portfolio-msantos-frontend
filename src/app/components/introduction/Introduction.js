edfa5d84f0ae95c95a689c6a0eae037a2ed00b3b
msantos-lab <msantosdev.lab@gmail.com>
2025-02-10 18:58:47 +0100

    Adds sanitization and dynamic integration of content coming from the API in the Introduction component.

--- src/app/components/introduction/Introduction.js
+++ src/app/components/introduction/Introduction.js
@@ -1,20 +1,27 @@
+import React from 'react';
 import LayersIcon from '@mui/icons-material/Layers';
-export default function Introduction() {
+import Typography from '@mui/material/Typography';
+import DOMPurify from 'dompurify';
+
+export default function Introduction({ data }) {
+    if (!data) return null; // Se os dados não existirem, não renderiza nada
+
+    // Sanitizando o conteúdo HTML para segurança
+    const sanitizedDescription = DOMPurify.sanitize(data.description ?? "No content");
+
     return (
         <section className="dark:bg-dark-primary bg-light-primary pt-28 pb-20" id="introduction">
             <div className="container max-w-6xl mx-auto px-4 md:py-8 flex flex-col items-center">
-                <p className="dark:text-gray-200  text-black mb-2">Olá, meu nome é </p>      
+                <p className="dark:text-gray-200 text-black mb-2">{data.eyebrow ?? "No content"}</p>      
                 <h1 className="text-gradient text-4xl md:text-5xl font-bold mb-4">
-                    Mônica Santos
+                    {data.title ?? "No content"}
                 </h1>
-                <p className="dark:text-gray-200 text-black mb-6 max-w-2xl text-center">
-                Com mais de uma década gerenciando projetos web, migrei para a programação para unir visão estratégica e habilidades técnicas.
-                </p>
-                <p className="dark:text-gray-200 text-black mb-6 max-w-2xl text-center">
-                Em 2024, concluí o Curso Técnico em Programação de Sistemas e, desde então, venho aprimorando minhas habilidades com projetos práticos.
-                </p>
+                <Typography
+                    className="dark:text-gray-200 text-black mb-6 max-w-2xl text-center"
+                    dangerouslySetInnerHTML={{ __html: sanitizedDescription }} 
+                />
                 <a href="#projects" className="btn-default">
-                    <p> <LayersIcon sx={{ fontSize: 25 }} /> Confira meus últimos trabalhos  </p>
+                    <p> <LayersIcon sx={{ fontSize: 25 }} /> {data.button_text ?? "No content"} </p>
                 </a>
             </div>
         </section>
