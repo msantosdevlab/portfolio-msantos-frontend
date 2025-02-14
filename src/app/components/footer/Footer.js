ccda94cdad68296dc7e2f03c7a12cd06737d5af7
msantos-lab <msantosdev.lab@gmail.com>
2025-02-14 20:02:07 +0100

    Update 'Menu' and 'Footer' to receive dynamic content from API

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -1,13 +1,13 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function Footer() {
+export default function Footer({ data }) {
     const currentYear = new Date().getFullYear();
 
     return(
-        <footer className="dark:bg-dark-primary bg-light-primary py-5" id="footer">
+        <footer className="dark:dark:bg-darkPrimary bg-light-primary py-5" id="footer">
             <div className="container max-w-6xl mx-auto text-sm flex justify-between">
-              <p className="dark:text-dark-footer text-light-footer ">&copy; Todos os direitos reservados - {currentYear}</p>
-              <a href="https://github.com/msantosdevlab" target='_blank' className="dark:text-dark-footer dark:hover:text-pinkLogo  text-light-footer hover:text-pinkLogo"> <GitHubIcon  sx={{ fontSize: 25 }} /></a>
+              <p className="dark:text-dark-footer text-light-footer ">{ data.text } - {currentYear}</p>
+              <a href={ data.icon_href } target='_blank' className="dark:text-dark-footer dark:hover:text-pinkLogo  text-light-footer hover:text-pinkLogo"> <GitHubIcon  sx={{ fontSize: 25 }} /></a>
            </div>
         </footer>
 
--- src/app/components/navbar/Navbar.js
+++ src/app/components/navbar/Navbar.js
@@ -1,10 +1,10 @@
-// components/Navbar.js
 import React, { useState, useEffect } from 'react';
 import { IconButton, Drawer, Button } from "@mui/material";
 import { Menu as MenuIcon, Close as CloseIcon } from "@mui/icons-material";
 import ThemeToggle from '../ThemeToggle';
+import LanguageToggle from '../LanguageToggle';
 
-export default function Navbar({ darkMode, toggleTheme }) {
+export default function Navbar({ darkMode, toggleTheme, language, setLanguage, menu }) {
   const [mobileOpen, setMobileOpen] = useState(false);
   const [activeSection, setActiveSection] = useState('');
 
@@ -29,24 +29,31 @@ export default function Navbar({ darkMode, toggleTheme }) {
     return () => sections.forEach((section) => observer.unobserve(section));
   }, []);
 
-
   return (
-    <header className="dark:bg-dark-primary bg-light-primary border-b dark:border-[#050505] border-gray-100 fixed w-full z-[1000] shadow-(--shadow-light)">
+    <header className="dark:bg-darkPrimary bg-lightPrimary border-b dark:border-[#050505] border-gray-100 fixed w-full z-[1000] shadow-(--shadow-light)">
       <div className="container mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8">
         <div className="text-2xl font-bold text-pinkLogo">
-            <a href="#introduction">M<span className="dark:text-gray-300  text-black ">SANTOS</span></a>
+          <a href="#introduction">M<span className="dark:text-gray-300  text-black ">SANTOS</span></a>
         </div>
 
-        <nav className="gap-8 font-bold uppercase items-center hidden md:flex">
-          {[
-            { id: 1, name: "introduction", label: "Olá" },
-            { id: 2, name: "projects", label: "Projetos" },
-            { id: 3, name: "linkedinprofile", label: "LinkedIn" },
-            { id: 4, name: "contact", label: "Contato" },
-          ].map(({ id, name, label }) => (
-            <a key={id} href={`#${name}`} className={activeSection === name ? 'text-pinkLogo' : 'dark:text-gray-300  text-black hover:text-pinkLogo dark:hover:text-pinkLogo'}>{label}</a>
+        <nav className="gap-8 font-bold uppercase items-center hidden md:flex cursor-pointer">
+          {menu
+          .sort((a, b) => a.order - b.order) // Ordena os itens com base no campo `order`
+          .map(({ name, label }) => (
+            <a
+              key={name}
+              href={`#${name}`}
+              className={activeSection === name ? 'text-pinkLogo' : 'dark:text-gray-300 text-black hover:text-pinkLogo dark:hover:text-pinkLogo'}
+            >
+              {label}
+            </a>
           ))}
+        <div className="flex gap-2">
           <ThemeToggle darkMode={darkMode} toggleTheme={toggleTheme} />
+          <LanguageToggle language={language} setLanguage={setLanguage} />
+        </div>
+
+
         </nav>
 
         <div className="block md:hidden">
@@ -54,24 +61,20 @@ export default function Navbar({ darkMode, toggleTheme }) {
         </div>
       </div>
       <Drawer anchor="right" open={mobileOpen} onClose={toggleDrawer(false)}>
-        <div className="dark:bg-dark-primary bg-light-secondary w-64 p-4 h-full">
+        <div className="dark:bg-darkPrimary bg-lightPrimary w-64 p-4 h-full">
           <div className="flex justify-between items-center mb-6">
             <div className="font-bold dark:text-gray-300 text-gray-700">Menu</div>
             <IconButton onClick={toggleDrawer(false)}>
-              <CloseIcon className="text-gray-800 dark:text-white" />
+              <CloseIcon className="text-gray-800 dark:text-white cursor-pointer" />
             </IconButton>
           </div>
           <nav className="flex flex-col md:hidden gap-5 uppercase font-bold">
-            {[
-              { id: 1, name: "introduction", label: "Olá" },
-              { id: 2, name: "projects", label: "Projetos" },
-              { id: 3, name: "linkedinprofile", label: "LinkedIn" },
-              { id: 4, name: "contact", label: "Contato" },
-            ].map(({ id, name, label }) => (
+            {menu.map(({ id, name, label }) => (
               <a key={id} href={`#${name}`} className="dark:text-gray-300 text-black hover:text-pinkLogo dark:hover:text-pinkLogo">{label}</a>
             ))}
-            <div className="w-1">
+            <div className="w-1 flex gap-4">
               <ThemeToggle darkMode={darkMode} toggleTheme={toggleTheme} />
+              <LanguageToggle language={language} setLanguage={setLanguage} />
             </div>
           </nav>
         </div> 
