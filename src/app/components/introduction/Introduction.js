5863143a96c76bc537655e245b102b704ac73ce7
msantos-lab <msantosdev.lab@gmail.com>
2025-02-14 12:02:31 +0100

    Refactor 'Introduction' component to use 'html-react-parser' and 'DOMPurify' for safer HTML rendering and upgrade classNames.

--- src/app/components/introduction/Introduction.js
+++ src/app/components/introduction/Introduction.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LayersIcon from '@mui/icons-material/Layers';
+import parse from "html-react-parser";
 import DOMPurify from 'dompurify';
 
 export default function Introduction({ data }) {
@@ -7,22 +8,26 @@ export default function Introduction({ data }) {
 
     // Sanitizando o conteúdo HTML para segurança
     const sanitizedDescription = DOMPurify.sanitize(data.description ?? "No content");
+    const description = parse(sanitizedDescription)
 
     return (
-        <section className="dark:bg-dark-primary bg-light-primary pt-28 pb-20" id="introduction">
-            <div className="container max-w-6xl mx-auto px-4 md:py-8 flex flex-col items-center">
-                <p className="dark:text-gray-200 text-black mb-2">{data.eyebrow ?? "No content"}</p>      
-                <h1 className="text-gradient text-4xl md:text-5xl font-bold mb-4">
-                    {data.title ?? "No content"}
-                </h1>
-                <p
-                    className="dark:text-gray-200 text-black mb-6 max-w-2xl text-center"
-                    dangerouslySetInnerHTML={{ __html: sanitizedDescription }} 
-                />
-                <a href="#projects" className="btn-default">
-                    <p> <LayersIcon sx={{ fontSize: 25 }} /> {data.button_text ?? "No content"} </p>
-                </a>
-            </div>
-        </section>
+      <section id="introduction" className="pt-28 pb-20 dark:bg-darkPrimary g-lightPrimary">
+        <div className="container mx-auto max-w-6xl px-4 md:py-8 flex flex-col items-center">
+          <p className="mb-2 text-black dark:text-gray-200">
+            {data.eyebrow ?? "No content"}
+          </p>
+          <h1 className="mb-4 font-bold text-5xl sm:text-6xl md:text-5xl lg:text-6xl">
+            {data.title ?? "No content"}
+          </h1>
+          <div className="mb-4 max-w-2xl text-center text-black dark:text-gray-200">
+            {description}
+          </div>
+          <a href="#projects" className="btn-default">
+            <span>
+              <LayersIcon sx={{ fontSize: 25 }} /> {data.button_text ?? "No content"}
+            </span>
+          </a>
+        </div>
+      </section>
     );
 }
