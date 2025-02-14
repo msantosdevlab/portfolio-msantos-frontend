4fea199c1243d66f83c48dfba5d484ef1590c8ab
msantos-lab <msantosdev.lab@gmail.com>
2025-02-14 19:53:06 +0100

    Change 'ThemeToggle'  background color

--- src/app/components/ThemeToggle.js
+++ src/app/components/ThemeToggle.js
@@ -4,7 +4,7 @@ export default function ThemeToggle({ darkMode, toggleTheme }) {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 rounded-lg bg-gray-100 dark:bg-gray-800 cursor-pointer"
+      className="p-2 rounded-lg bg-lightSecondary dark:bg-darkSecondary cursor-pointer"
       aria-label={darkMode ? "Activate light mode" : "Activate dark mode"}
     >
       {darkMode ? <Sun className="text-yellow-500" /> : <Moon className="text-gray-600" />}
