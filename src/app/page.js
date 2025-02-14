65377b90282f7326224c4a77bb5cd6f7779d1f61
msantos-lab <msantosdev.lab@gmail.com>
2025-02-14 19:58:42 +0100

    Refactor 'Home' component to fetch content based on language from 'Navbar' and retrieve 'Menu' and 'Footer' data from API

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-import { useFetchData } from "@/hooks/useFetchData"
+import { useFetchData } from "@/hooks/useFetchData";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { CssBaseline, LinearProgress, Typography  } from '@mui/material';
+import { CssBaseline, LinearProgress, Typography } from '@mui/material';
 import Navbar from '@/app/components/navbar/Navbar';
 import Introduction from '@/app/components/introduction/Introduction';
 import { ProjectsSection } from '@/app/components/projects/ProjectsSection';
@@ -14,7 +14,10 @@ import 'aos/dist/aos.css';
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(true);
-  const { data, loading, error } = useFetchData("content");
+  const [language, setLanguage] = useState("pt"); // Estado para controlar o idioma
+
+  // Chama o useFetchData passando o idioma selecionado
+  const { data, loading, error } = useFetchData("content", language);
 
   useEffect(() => {
     // Carregar o script do Tawk
@@ -37,7 +40,7 @@ export default function Home() {
       document.head.removeChild(script);
     };
   }, []); // Só roda uma vez quando o componente é montado
-  
+
   useEffect(() => {
     document.documentElement.classList.add('dark');
     AOS.init({ once: false });
@@ -52,32 +55,32 @@ export default function Home() {
   if (loading || error) {
     return (
       <section className="dark:dark:bg-darkPrimary bg-light-primary loader-area flex flex-col justify-center items-center min-h-screen gap-2">
-      <LinearProgress
-        sx={{
-        width: "50%",
-          height: "1px",
-          borderRadius: "4px",
-          background: "linear-gradient(95deg, var(--color-lightBlue) 15%, var(--color-lightPink) 45%, var(--color-secondaryAlt) 75%, var(--color-secondary))",
-        }}
-      />
-      <Typography
-      sx={{
-        fontSize: "1rem",
-        fontWeight: "bold",
-        animation: "blink 3.5s infinite"
-      }}
-      className="dark:text-gray-200 text-black"
-      >
-        Loading...
-      </Typography>
-    </section>
+        <LinearProgress
+          sx={{
+            width: "50%",
+            height: "1px",
+            borderRadius: "4px",
+            background: "linear-gradient(95deg, var(--color-lightBlue) 15%, var(--color-lightPink) 45%, var(--color-secondaryAlt) 75%, var(--color-secondary))",
+          }}
+        />
+        <Typography
+          sx={{
+            fontSize: "1rem",
+            fontWeight: "bold",
+            animation: "blink 3.5s infinite"
+          }}
+          className="dark:text-gray-200 text-black"
+        >
+          Loading...
+        </Typography>
+      </section>
     );
   }
 
   // Extraindo os dados da API
-  const { introduction, project_title, project_categories, projects, linkedin, contact } = data || {};
+  const { menu, introduction, project_title, project_categories, projects, linkedin, contact, footer } = data || {};
 
-  // Ciando um tema customizado com MUI
+  // Criando um tema customizado com MUI
   const theme = createTheme({
     palette: {
       mode: darkMode ? "dark" : "light",
@@ -96,14 +99,16 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Navbar darkMode={darkMode} toggleTheme={toggleTheme} />
-      {introduction && <Introduction data={introduction} />}
+      {/* Passando o estado language e a função setLanguage para Navbar */}
+      <Navbar darkMode={darkMode} toggleTheme={toggleTheme} language={language} setLanguage={setLanguage} menu={ menu } />
+      
+      {introduction && <Introduction data={ introduction } />}
       {project_title && project_categories && projects && (
         <ProjectsSection data={project_title} projects={projects} categories={project_categories} />
       )}
-      {linkedin && <LinkedinProfile data={linkedin} />}
-      {contact && <Contact data={contact} />}
-      <Footer />
-  </ThemeProvider>
+      {linkedin && <LinkedinProfile data={ linkedin } />}
+      {contact && <Contact data={ contact } />}
+      {footer && <Footer data={ footer }/>}
+    </ThemeProvider>
   );
 }
