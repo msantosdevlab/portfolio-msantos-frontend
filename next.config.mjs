4d5cfc6647b6659f1cb0ec208a439fefb945a960
msantos-lab <msantosdev.lab@gmail.com>
2025-02-14 12:09:35 +0100

    Add environment variable `NEXT_PUBLIC_IMAGE` for image hostname configuration in `next.config.mjs`

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http', 
+        hostname: new URL(process.env.NEXT_PUBLIC_IMAGE).hostname, 
+      },
+    ],
+  },
+};
 
 export default nextConfig;
