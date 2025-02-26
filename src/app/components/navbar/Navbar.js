0d1970043ba9c28e686ae13b9e1418b4c8f03e37
msantos-lab <msantosdev.lab@gmail.com>
2025-02-26 18:58:56 +0100

    Replaces the use of <a> with <Link> from Next.js for internal navigation

--- src/app/components/navbar/Navbar.js
+++ src/app/components/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { IconButton, Drawer, Button } from "@mui/material";
 import { Menu as MenuIcon, Close as CloseIcon } from "@mui/icons-material";
 import ThemeToggle from '../ThemeToggle';
@@ -33,21 +34,31 @@ export default function Navbar({ darkMode, toggleTheme, language, setLanguage, m
     <header className="dark:bg-darkPrimary bg-lightPrimary border-b dark:border-[#050505] border-gray-100 fixed w-full z-[1000] shadow-(--shadow-light)">
       <div className="container mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8">
         <div className="text-2xl font-bold text-pinkLogo">
-          <a href="#introduction">M<span className="dark:text-gray-300  text-black ">SANTOS</span></a>
+          <a href="#introduction"><span className="dark:text-gray-300  text-black ">M</span>SANTOS<span className="dark:text-gray-300  text-black ">LAB</span></a>
         </div>
 
         <nav className="gap-8 font-bold uppercase items-center hidden md:flex cursor-pointer">
-          {menu
-          .sort((a, b) => a.order - b.order) // Ordena os itens com base no campo `order`
-          .map(({ name, label }) => (
-            <a
-              key={name}
-              href={`#${name}`}
-              className={activeSection === name ? 'text-pinkLogo' : 'dark:text-gray-300 text-black hover:text-pinkLogo dark:hover:text-pinkLogo'}
-            >
-              {label}
-            </a>
-          ))}
+          {menu && menu.length > 0 ? (
+            menu
+              .sort((a, b) => a.order - b.order)
+              .map(({ name, label }) => (
+                <Link
+                  key={name}
+                  href={`#${name}`}
+                  className={
+                    activeSection === name
+                      ? 'text-pinkLogo'
+                      : 'dark:text-gray-300 text-black hover:text-pinkLogo dark:hover:text-pinkLogo'
+                  }
+                >
+                  {label}
+                </Link>
+              ))
+              ) : (
+            <span>Menu</span>
+          )}
+
+
         <div className="flex gap-2">
           <ThemeToggle darkMode={darkMode} toggleTheme={toggleTheme} />
           <LanguageToggle language={language} setLanguage={setLanguage} />
@@ -69,9 +80,15 @@ export default function Navbar({ darkMode, toggleTheme, language, setLanguage, m
             </IconButton>
           </div>
           <nav className="flex flex-col md:hidden gap-5 uppercase font-bold">
-            {menu.map(({ id, name, label }) => (
-              <a key={id} href={`#${name}`} className="dark:text-gray-300 text-black hover:text-pinkLogo dark:hover:text-pinkLogo">{label}</a>
-            ))}
+          {menu && menu.length > 0 ? (
+              menu.map(({ id, name, label }) => (
+                <Link key={id} href={`#${name}`} className="dark:text-gray-300 text-black hover:text-pinkLogo dark:hover:text-pinkLogo">
+                  {label}
+                </Link>
+              ))
+            ) : (
+              <span>Menu</span>
+            )}
             <div className="w-1 flex gap-4">
               <ThemeToggle darkMode={darkMode} toggleTheme={toggleTheme} />
               <LanguageToggle language={language} setLanguage={setLanguage} />
